d668571a1899997c9c7628b5ed0c1fad0554bcf6
konyalaLakshmi <130556400+konyalaLakshmi@users.noreply.github.com>
2024-03-22 11:17:11 +0530

    Update index.js

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,8 +1,8 @@
-import {Component} from 'react'
-import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom'
+import React, { Component } from 'react';
+import Cookies from 'js-cookie';
+import { Redirect } from 'react-router-dom';
 
-import './index.css'
+import './index.css';
 
 class LoginForm extends Component {
   state = {
@@ -10,49 +10,42 @@ class LoginForm extends Component {
     password: '',
     showSubmitError: false,
     errorMsg: '',
-  }
+  };
 
   onSubmitSuccess = jwtToken => {
-    const {history} = this.props
+    const { history } = this.props;
     Cookies.set('jwt_token', jwtToken, {
       expires: 30,
       path: '/',
-    })
-    history.replace('/')
-  }
+    });
+    history.replace('/');
+  };
 
   onSubmitFailure = errorMsg => {
-    this.setState({showSubmitError: true, errorMsg})
-  }
+    this.setState({ showSubmitError: true, errorMsg });
+  };
 
   onSubmitForm = async event => {
-    event.preventDefault()
-    const {username, password} = this.state
-    const userDetails = {username, password}
-    const url = 'https://apis.ccbp.in/login'
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(userDetails),
-    }
-    const response = await fetch(url, options)
-    const data = await response.json()
-    if (response.ok === true) {
-      this.onSubmitSuccess(data.jwt_token)
+    event.preventDefault();
+    const { username, password } = this.state;
+    if (username === 'lakshmi' && password === 'lakshmi@2022') {
+      // Simulating a successful login with a fake JWT token
+      this.onSubmitSuccess('fake_jwt_token');
     } else {
-      this.onSubmitFailure(data.error_msg)
+      this.onSubmitFailure('Invalid username or password');
     }
-  }
+  };
 
   onEnterUsername = event => {
-    this.setState({username: event.target.value})
-  }
+    this.setState({ username: event.target.value });
+  };
 
   onChangePassword = event => {
-    this.setState({password: event.target.value})
-  }
+    this.setState({ password: event.target.value });
+  };
 
   renderUsername = () => {
-    const {username} = this.state
+    const { username } = this.state;
 
     return (
       <>
@@ -68,11 +61,11 @@ class LoginForm extends Component {
           onChange={this.onEnterUsername}
         />
       </>
-    )
-  }
+    );
+  };
 
   renderPassword = () => {
-    const {password} = this.state
+    const { password } = this.state;
 
     return (
       <>
@@ -88,14 +81,14 @@ class LoginForm extends Component {
           onChange={this.onChangePassword}
         />
       </>
-    )
-  }
+    );
+  };
 
   render() {
-    const {showSubmitError, errorMsg} = this.state
-    const jwtToken = Cookies.get('jwt_token')
+    const { showSubmitError, errorMsg } = this.state;
+    const jwtToken = Cookies.get('jwt_token');
     if (jwtToken !== undefined) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
 
     return (
@@ -117,8 +110,8 @@ class LoginForm extends Component {
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default LoginForm
+export default LoginForm;
